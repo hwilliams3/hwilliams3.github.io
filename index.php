1286941c89a58ce15743f24163d6d14caf198f00
Henry Williams <henry.williams@northumbria.ac.uk>
2019-04-26 22:42:06 +0100

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <title>Portfolio</title>
 </head>
 <body>
+  
+  <?php echo "test 1234" ?>
 
 <h1>Xamarin Forms Application (C#) - Crime Stats</h1>
 <p>Crime Stats shows you the crime rate around your area.</p>
